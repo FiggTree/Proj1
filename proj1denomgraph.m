9e017257e78cfeff7d0e57864bb21bf04d0bc09a
FiggTree <50163056+FiggTree@users.noreply.github.com>
2019-05-11 13:47:22 -0700

    Create proj1denomgraph.m

---
+++ proj1denomgraph.m
@@ -0,0 +1,8 @@
+%figure
+%plot(H2Omolamt,H2molamt./9,'r',H2Omolamt,H2molfrac,'b')
+%hold on
+%ylim([0 0.18]);
+%xlabel('Mole Fraction of H_2O')
+%ylabel('H_2 Amount and Fraction')
+%legend('H_2 Mole Amount (scaled)','H_2 Mole Fraction')
+%title('Comparison of H_2 Amount and Fraction versus Initial H_2O')
